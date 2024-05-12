cb3083e97c79cecbf55463a8df30a8f9ab25eb2e
aabytt <84480313+aabytt@users.noreply.github.com>
2024-05-12 23:35:15 +0300

    Update MainPanel.js

--- frontend/views/MainPanel.js
+++ frontend/views/MainPanel.js
@@ -26,7 +26,6 @@ module.exports = kind({
   name: 'MainPanel',
   kind: Panel,
   title: 'webOS Aerial Screensaver',
-  titleBelow: "webosbrew.org",
   headerType: 'medium',
   components: [
     {kind: FittableColumns, fit: true, components: [
